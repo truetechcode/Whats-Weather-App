963306515231dc4d8566b64df81da5ab2f6f2498
TERVER AOSU <terveraosu@gmail.com>
2019-09-09 00:00:58 +0100

    added feature to change temperature scale

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,12 +1,28 @@
-import { loadWeather, container } from './weatherModule';
+import { loadWeather, container, tempArray } from './weatherModule';
 
 const form = document.querySelector('div[weather] #weather-form');
+let tempCount = 1;
+const clickB = () => {
+  const tempButton = document.querySelector('#temp-button');
+  const tempHolder = document.querySelector('#temp-holder');
+  tempButton.addEventListener('click', (e) => {
+    if(tempCount % 2 === 0){
+      tempHolder.innerHTML = tempArray[0]
+    }else{
+      tempHolder.innerHTML = tempArray[1]
+    }
+    tempCount++
+    })
+}
 
 form.addEventListener('submit', (event) => {
   const city = document.querySelector('div[weather] form > input[name="city"]').value.toLowerCase();
   const country = document.querySelector('div[weather] form > input[name="country"]').value.toLowerCase();
   container.innerHTML = "";
   loadWeather(city, country);
+  setTimeout(() => {
+    clickB();    
+  }, 3000);
   // form.reset();
   event.preventDefault();
 });
--- src/javascript/weatherModule.js
+++ src/javascript/weatherModule.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('div[weather] .content');
 const location = document.createElement('div');
 const img = document.createElement('img');
-location.classList.add('location')
+location.classList.add('location');
+let tempArray = [];
 
 const loadWeather = (ci, co) => {
   const url = `https://api.aerisapi.com/observations/${ci},${co}?client_id=fXqdhQQAlw2yTQGiX179N&client_secret=k0y4NauNd3aeeuJljCS2DXZMClP3m9jQN5HQCQtK`;
@@ -10,10 +11,11 @@ const loadWeather = (ci, co) => {
     .then((response) => {
       if (response.error === null) {
         const {
-          weather, tempF, tempC, humidity, icon, isDay, windDir,
+          weather, tempF, tempC, icon, isDay,
         } = response.response.ob;
+        tempArray = [`${tempF}&#8457;`,`${tempC}&#8451;`];
         const { lat, long } = response.response.loc;
-        const content = `          <p class="header">
+        const content = `<p class="header">
         Location: <span>${ci}, ${co}</span>
       </p>
       <p>            
@@ -24,7 +26,7 @@ const loadWeather = (ci, co) => {
       </p>
       <p class="description">
         <p>Weather: <span>${weather.toLowerCase()}</span></p>
-        <p>Temperature: <span>${tempF}&#8457; - ${tempC}&#8451;</span> <button>&#8457; / &#8451;</button></p>            
+        <p>Temperature: <span id="temp-holder">${tempF}&#8457;</span> <button id="temp-button">&#8457; / &#8451;</button></p>            
       </p>`;
         location.innerHTML = content;
         img.setAttribute('src', 'https://cdn.aerisapi.com/wxicons/v2/' + icon);
@@ -32,11 +34,14 @@ const loadWeather = (ci, co) => {
         container.appendChild(location)
         container.appendChild(img)
       } else {
-        container.innerHTML = response.error.description;// `The weather data for ${ci}, ${co} is ${weather.toLowerCase()} with a temperature of ${tempF}&deg;`;
+        container.innerHTML = `<p>Sorry! The weather data for ${ci}, ${co} is not available, try again later.</p>`;
       }
     })
-    .catch((error) => console.log('Error:', error));
+    .catch((error) => {
+      container.innerHTML = `<p>Sorry! The weather data for ${ci}, ${co} is not available, try again later.</p>`;
+    });
 };
 
 
-export { loadWeather, container };
+
+export { loadWeather, container, tempArray};
